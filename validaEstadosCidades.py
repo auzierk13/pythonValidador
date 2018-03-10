9a2f1f09674570e830d145ad85c0bbfc8bf83af1
Bruno Auzier <auzierk13@gmail.com>
2018-03-09 23:55:26 -0400

    Valida estados e cidades corretos

--- validaEstadosCidades.py
+++ validaEstadosCidades.py
@@ -1,22 +1,38 @@
-#manipulador= open('Enunciado.txt','r')
-manipulador= open('Estados+CIdades.txt','r', encoding='UTF8')
+estadosCidadesErrados  = open('Estados+CIdades(erro).txt','r',encoding='UTF8')
+estadosCidadesCorretos = open('Estados+CIdades.txt','r', encoding='UTF8')
+
 
 print("Metodo read () : \n")
-a= manipulador.readlines()
-#print(manipulador.readlines())
+#Entrada de dados
+arqCorreto= estadosCidadesCorretos.readlines()
+arqErrado= estadosCidadesErrados.readlines()
+
 indexLinha=0
-for linha in a:
+for linha in arqErrado:
     indexLinha+=1
-    linha = linha.rstrip()
-    print(str(indexLinha) +": "+linha)
+    linha = linha.rstrip() #Limpa linha em branco
+    for linhaCerta in arqCorreto:
+        linhaCerta = linhaCerta.rstrip() #Limpa linha em branco
+        ##******Estado Correto********##
+        if(linha.find(linhaCerta)!= -1): #print("Estado correto")
+            print("Estado correto")
+            print(str(indexLinha) +" "+linha+": "+linhaCerta)
+            break
+        ##******End Estado Correto ****##
+        ##*****Linha em branco *****##
+        if(linha == ""):
+            print("Linha em branco")
+            print("Erro na linha "+str(indexLinha) +" :Linha em Branco")
+            break
+        ##*****End Linha em branco *****##
+        else:
+            if indexLinha > 50:
+                break
+    estadosCidadesCorretos.seek(0) #Volta para o inicio do arquivo
+    """
     if indexLinha > 50:
         break
+    """ 
 
-manipulador.seek(0) #Volta para o inicio do arquivo
-
-"""
-for linha in manipulador:
-    linha = linha.rstrip()
-    print(linha)
-"""
-manipulador.close
+estadosCidadesErrados.close
+estadosCidadesCorretos.close
