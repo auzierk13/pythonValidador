42c2fc2258ed2122c2390fb7f0ba7dfd94477ff8
Bruno Auzier <auzierk13@gmail.com>
2018-03-10 09:14:46 -0400

    Adicionando funcao para verificar se sigla existe

--- validaEstadosCidades.py
+++ validaEstadosCidades.py
@@ -1,4 +1,4 @@
-estadosCidadesErrados  = open('Estados+CIdades(erro).txt','r',encoding='UTF8')
+estadosCidadesErrados  = open('Estados+CIdades(erro1).txt','r',encoding='UTF8')
 estadosCidadesCorretos = open('Estados+CIdades.txt','r', encoding='UTF8')
 
 
@@ -12,33 +12,57 @@ for linha in arqErrado:
     indexLinha+=1
     linha = linha.rstrip() #Limpa linha em branco
     
-    
+    ##*****Linha em branco *****##
+    if(linha == ""):
+        #print("Linha em branco")
+        print("Erro na linha "+str(indexLinha) +":Linha em Branco")
+        break
+    ##*****End Linha em branco *****##
+    estado,sigla,cidade = linha.split("|")
+    hasSigla = False
+
+    ##***** Sigla escrita errada *****##
+    if(len(sigla)>2):
+        print("Erro na linha "+str(indexLinha) +":"+sigla+" sigla escrita errada com > 2 letras")
+        hasSigla=True
+    if(len(sigla) <2):
+        print("Erro na linha "+str(indexLinha) +":"+sigla+" sigla escrita errada com < 2 letras")
+        hasSigla=True
+    ##***** End Sigla escrita errada *****#
+        
     for linhaCerta in arqCorreto:
         linhaCerta = linhaCerta.rstrip() #Limpa linha em branco
         ##******Estado Correto********##
         if(linha.find(linhaCerta)!= -1): #print("Estado correto")
             #print("Estado correto")
-            print(str(indexLinha) +" "+linha+": "+linhaCerta)
+            #print(str(indexLinha) +" "+linha+": "+linhaCerta)
+            hasSigla=True #Existe Sigla 
             break
         ##******End Estado Correto ****##
-        ##*****Linha em branco *****##
-        if(linha == ""):
-            #print("Linha em branco")
-            print("Erro na linha "+str(indexLinha) +" :Linha em Branco")
-            break
-        ##*****End Linha em branco *****##
-        estado,sigla,cidade = linha.split("|")
-        ##***** Sigla escrita errada *****##
-        if(len(sigla)>2):
-            print("Erro na linha "+str(indexLinha) +":"+sigla+" sigla escrita errada com > 2 letras")
-        if(len(sigla) <2):
-            print("Erro na linha "+str(indexLinha) +":"+sigla+" sigla escrita errada com < 2 letras")
 
-        ##***** End Sigla escrita errada *****#
-        else:
-            if indexLinha > 50:
-                break
+        estadoCorreto,siglaCorreta,cidadeCorreta = linhaCerta.split("|")
+        if(sigla == siglaCorreta):
+            hasSigla=True
+            
+
+
+        
+
+
+        
+        ##***** Sigla não existe (Duas letra mais sigla não encontrado)*****##
+            
+            
+        ##***** Sigla não existe (Duas letra mais sigla não encontrado)*****##
+
+
+        
+
+    
     estadosCidadesCorretos.seek(0) #Volta para o inicio do arquivo
+    if(not hasSigla):
+        print("Erro na linha "+str(indexLinha) +":"+sigla+ " sigla nao encontrada")
+        hasSigla = False
     """
     if indexLinha > 50:
         break
