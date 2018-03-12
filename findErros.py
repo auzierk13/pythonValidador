d9ade8844083587a80701ba6c1cfefc9fff9986e
Bruno Auzier <auzierk13@gmail.com>
2018-03-12 12:44:14 -0400

    Adicionando funcao para validar cidade existe

--- findErros.py
+++ findErros.py
@@ -1,15 +1,16 @@
 import re
 
+estadosCidadesCorretos = open('Estados+CIdades.txt','r', encoding='UTF8')
 
 #grupo 1
 inicioCorreto1 = open("parseCorreto1.txt", 'r', encoding='UTF8')
 inicioErrado1 = open("parseErrado1.txt", 'r', encoding='UTF8')
 
-
+#grupo 2
 meioCorreto2 = open("parseCorreto2.txt", 'r', encoding='UTF8')
 meioErrado2 = open("parseErrado2.txt", 'r', encoding='UTF8')
 
-
+#grupo 3
 fimCorreto3 = open("parseCorreto3.txt", 'r', encoding='UTF8')
 fimErrado3 = open("parseErrado3.txt", 'r', encoding='UTF8')
 
@@ -25,7 +26,7 @@ log = open("log.txt", 'a', encoding='UTF8')
 def buscaErrosGenericosGrupo(arqCerto, arqErrado):
     arqCerto.seek(0)
     arqErrado.seek(0)
-
+    indexPonto = 0 
     print("Inicio da busca")
     for linhaErrada in arqErrado:
         #print(linhaErrada)
@@ -50,7 +51,9 @@ def buscaErrosGenericosGrupo(arqCerto, arqErrado):
                 #print("Não deveria imprimir no 4 ["+linhaErrada+"]["+linhaCerta+"]")
                 linhaSemErros = True
                 break
-            
+        if(  indexPonto%100==0):
+            print(".")
+        indexPonto+=1
         arqCerto.seek(0)
         if(not linhaSemErros): #Foi encontrado algum erro
             print(linhaErrada)
@@ -83,14 +86,14 @@ def erroSigla(dicEstados):
         index,estado,sigla,cidade = siglaTest.split("|")
         #print(index+" "+sigla)
         hasSiglaCorreta=False
-        
+        linhaSigla = estado+"|"+sigla+ "|"+cidade
         for siglaCorreta in listSiglas:
             if(siglaCorreta == sigla):
                 hasSiglaCorreta=True #Sigla existe esta correta
                 for estados in listEstadosSigla:
                     estadosValidos,siglaValidos = estados
                     if((estado==estadosValidos) and (sigla!=siglaValidos)):
-                        error = index+"|Erro na linha "+index+"["+siglaTest+"]: Sigla "+sigla+" não confere com o estado deveria ser "+siglaValidos+"." 
+                        error = index+"|Erro na linha "+index+"["+linhaSigla+"]: Sigla "+sigla+" não confere com o estado deveria ser "+siglaValidos+"." 
                         print(error)
                         log.write(error+"\n")
                         break
@@ -98,7 +101,7 @@ def erroSigla(dicEstados):
                 break #fim do loop listSiglas
 
         if(not hasSiglaCorreta):
-            linhaSigla = estado+"|"+sigla+ "|"+cidade
+            
             print(linhaSigla)
             if(len(sigla)!=2):
                 if(len(sigla) < 2):
@@ -125,22 +128,65 @@ def erroSigla(dicEstados):
 
             
         hasSiglaCorreta=False
-        
+    spaceOfErrors.seek(0)  
     
 
+def erroCidade():
+    #Verificar se cidade existe
+    estadosCidadesCorretos.seek(0)
+    spaceOfErrors.seek(0)
+
     
+    linhaPonto = 0
+    for cidadeTest in spaceOfErrors:
+        cidadeTest = cidadeTest.rstrip() #Limpa linha em branco
+        index,estado,sigla,cidade = cidadeTest.split("|")
+        #print(index+" "+cidade)
+        hasCidadeCorreta=False
+        linhaCidade = estado+"|"+sigla+ "|"+cidade
+
+        for linhaCorreta in estadosCidadesCorretos:
+            linhaCorreta = linhaCorreta.rstrip() #Limpa linha em branco
+            estadoCorreto,siglaCorreto,cidadeCorreto = linhaCorreta.split("|")
+
+            
+            if(cidade == cidadeCorreto):
+                hasCidadeCorreta=True
+                #print("Mostra linha 1 "+ linhaCorreta)
+                break
+
+
+        if((linhaPonto % 100) == 0):
+            print (".")
+
+
+        linhaPonto+=1
+
+        if(not hasCidadeCorreta ):
+            error = index+"|Erro na linha "+index+"["+linhaCidade+"]:Cidade "+cidade+" nao existe." 
+            print(error)
+            log.write(error+"\n")
+
+
+            
+        estadosCidadesCorretos.seek(0)
+
+        
+    spaceOfErrors.seek(0)
+
     
 def mainErrosGenericos(dicEstados):
    #Busca em grupo 1 
    buscaErrosGenericosGrupo(inicioCorreto1,inicioErrado1)
 
    #Busca em grupo 2
-   #buscaErrosGenericosGrupo(meioCorreto2,meioErrado2)
+   buscaErrosGenericosGrupo(meioCorreto2,meioErrado2)
 
    #Busca em grupo 3
-   #buscaErrosGenericosGrupo(fimCorreto3,fimErrado3)
+   buscaErrosGenericosGrupo(fimCorreto3,fimErrado3)
 
    erroSigla(dicEstados)
+   erroCidade()
   
 
    
